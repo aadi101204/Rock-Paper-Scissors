7adf44ce5d2761c66a573a371f38cf26dcf2d9b1
aadi101204 <113494774+aadi101204@users.noreply.github.com>
2024-05-27 20:02:35 +0530

    first commit

--- system.js
+++ system.js
@@ -1,43 +1,43 @@
 var pcChoice = null;
 var choice = null;
 var options = ["Rock", "Paper", "Scissors"];
-images = [
-  '"./Assets/rock.png"',
-  '"./Assets/paper.jpg"',
-  '"./Assets/scissors.jpg"',
+var images = [
+  "./Assets/rock.png",
+  "./Assets/paper.jpg",
+  "./Assets/scissors.jpg",
 ];
 var userInput = null;
 
 function computerChoice() {
   pcChoice = Math.floor(Math.random() * options.length);
   choice = options[pcChoice];
-  img = document.getElementsByClassName("AIselectImage");
-  img.src = images[pcChoice];
-  x = document.getElementsByClassName("AIselect");
+  var image = document.getElementsByClassName("photo")[0];
+  image.src = images[pcChoice];
+  var x = document.getElementsByClassName("AIselect");
   x[0].innerHTML = choice;
   generateResult();
 }
 
-function option(SelectedOption) {
-  userInput = SelectedOption;
+function option(selectedOption) {
+  userInput = selectedOption;
+  console.log("User selected:", userInput);
   computerChoice();
 }
 
-/*function generateResult() {
-  y = document.getElementsByClassName("result");
+function generateResult() {
+  var y = document.getElementsByClassName("Result");
+  console.log("Computer selected:", choice);
+
   if (choice == userInput) {
-    y[0].innerHTML = "draw";
-    return;
-  } else if (
-    (userInput == "rock" && choice == "scissors") ||
-    (userInput == "paper" && choice == "rock") ||
-    (userInput == "scissors" && choice == "paper")
-  ) {
-    y[0].innerHTML = "you Win";
-    return;
+    y[0].innerHTML = "Draw";
+  } else if (userInput == "Rock" && choice == "Scissors") {
+    y[0].innerHTML = "You Win!";
+  } else if (userInput == "Paper" && choice == "Rock") {
+    y[0].innerHTML = "You Win!";
+  } else if (userInput == "Scissors" && choice == "Paper") {
+    y[0].innerHTML = "You Win!";
   } else {
-    y[0].innerHTML = "you lose!";
-    return;
+    y[0].innerHTML = "You Lose!";
   }
+  return;
 }
-*/
